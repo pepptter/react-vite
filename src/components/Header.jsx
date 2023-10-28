2051b652c8d14bc1793ad62a4fe8241504fdb283
pepptter <143556865+pepptter@users.noreply.github.com>
2023-10-28 20:14:00 +0200

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import image from '@images/react.svg'
+
+const Header = () => {
+  return (
+    <div>
+        <img src={image} />
+    </div>
+  )
+}
+
+export default Header
