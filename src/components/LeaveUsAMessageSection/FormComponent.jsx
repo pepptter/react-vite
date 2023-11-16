4fa7b9785845977723f6eaafd67bd2d0b6d65a72
pepptter <143556865+pepptter@users.noreply.github.com>
2023-11-16 02:48:29 +0100

    show error messages, success/error submit message

--- src/components/LeaveUsAMessageSection/FormComponent.jsx
+++ src/components/LeaveUsAMessageSection/FormComponent.jsx
@@ -2,9 +2,12 @@ import { Formik, useFormik } from 'formik'
 import React, { useState } from 'react'
 import * as Yup from 'yup';
 
+
 const FormComponent = () => {
 
-    const [errorMessage, setErrorMessage] = useState('')
+    const [submitMessage, setSubmitMessage] = useState({text:'', type:''})
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
     const form = useFormik( {
         initialValues: {
             name: '',
@@ -39,32 +42,26 @@ const FormComponent = () => {
             })
                 switch(response.status){
                 case 200:
-                    alert('Status code 200! Message sent!')
+                    setSubmitMessage({ text: 'Message sent, Thank you!', type: 'success' })
+                    setIsFormSubmitted(true);
                     break
                 default:
                     const result = await response.json()
-                    alert(result.message)
+                    setSubmitMessage({ text: result.message, type: 'error' })
+                    setIsFormSubmitted(true);
                     break
-
             }
         } catch (error) {
-            console.error('Error:', error)
-            setErrorMessage(`An unexpected error occurred: ${error.message}`);
-            alert(`An unexpected error occurred: ${error.message}`);
+            console.error('Error:', error);
+            setSubmitMessage({ text: `An unexpected error occurred: ${error.message}`, type: 'error' });
         }
-
     };
 
 
   return (
         <>
-
-
-
-
-                <form id="message-form" className="message-form" noValidate onSubmit={form.handleSubmit}>
-                    <div className="one-row-form">
-
+            <form id="message-form" className="message-form" noValidate onSubmit={form.handleSubmit}>
+                <div className="one-row-form">
                     <input
                         className={`${form.touched.name && form.errors.name ? 'error' : ''}`}
                         id="name"
@@ -75,10 +72,10 @@ const FormComponent = () => {
                         onChange={form.handleChange}
                         onBlur={form.handleBlur}
                     />
-                    </div>
-                    <div className="one-row-form">
+                </div>
+                <div className="one-row-form">
                     <input
-                        className={`${form.touched.email && form.errors.email ? 'error' : ''}`}
+                        className={`${form.touched.email && form.errors.email ? 'error': ''}`}
                         id="email"
                         type="email"
                         name="email"
@@ -87,20 +84,44 @@ const FormComponent = () => {
                         onChange={form.handleChange}
                         onBlur={form.handleBlur}
                     />
-                    </div>
-                    <div className="multi-row-form">
-                        <textarea
-                        className={`${form.touched.message && form.errors.message ? 'error' : ''}`}
-                        id="message" name="message" placeholder="Your Message* "
-                        value={form.values.message}
-                        onChange={form.handleChange}
-                        onBlur={form.handleBlur}>
-                        </textarea>
-                    </div>
-                    <button type="submit" className="btn-yellow" >Submit</button>
+                </div>
+                <div className="multi-row-form">
+                    <textarea
+                    className={`${form.touched.message && form.errors.message ? 'error' : ''}`}
+                    id="message" name="message" placeholder="Your Message* "
+                    value={form.values.message}
+                    onChange={form.handleChange}
+                    onBlur={form.handleBlur}>
+                    </textarea>
+                </div>
+                <button type="submit" className="btn-yellow" >Submit</button>
 
-                </form>
-
+            </form>
+            {isFormSubmitted ? (
+                <div className='flex-class'>
+                    <div className={`submit-message ${submitMessage.type === 'success' ? 'green' : 'red'}`}>
+                        {submitMessage.text}
+                    </div>
+                </div>
+            ) : (
+                Object.keys(form.errors).some((fieldName) => form.touched[fieldName] && form.errors[fieldName]) && (
+                    <div className='flex-class row'>
+                        <div className="additional-message">
+                            Please correct the following error(s) to be able to submit your message:
+                        </div>
+                        {Object.keys(form.errors).map((fieldName, index) => {
+                            if (form.touched[fieldName] && form.errors[fieldName]) {
+                                return (
+                                    <div key={index} className="error-message">
+                                        {form.errors[fieldName]}
+                                    </div>
+                                );
+                            }
+                            return null;
+                        })}
+                    </div>
+                )
+            )}
         </>
     )
 }
